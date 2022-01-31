35a88e74fc73cea104adc6994728d5680772fcbe
DePacifier <milkybekele@gmail.com>
2022-01-31 12:21:36 +0300

    updated styles for menu-items and our package to the latest version

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -7,12 +7,14 @@ const MenuItem = ({ title, imageUrl, size }) => {
     <div className={`${size} menu-item`}>
       <div
         className="background-image"
-        style={{ backgroundImage: `url(${imageUrl})` }}
-      />
+        style={{
+          backgroundImage: `url(${imageUrl})`,
+        }}
+      />{" "}
       <div className="content">
-        <h1 className="title">{title.toUpperCase()}</h1>
-        <span className="subtitle">SHOP NOW</span>
-      </div>
+        <h1 className="title"> {title.toUpperCase()} </h1>{" "}
+        <span className="subtitle"> SHOP NOW </span>{" "}
+      </div>{" "}
     </div>
   );
 };
