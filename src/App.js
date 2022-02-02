ee9b31769cc5a86956fbdb727b1a6f1bbf3b1f24
DePacifier <milkybekele@gmail.com>
2022-02-02 12:16:20 +0300

    implemented firebase util to store authenticated users into our firestore database and enable email based sign-up

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ import LoginPage from "./pages/login/login.component";
 import Header from "./components/header/header.component";
 
 // Firebase Util Imports
-import { auth } from "./firebase/firebase.utils";
+import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 
 class App extends React.Component {
   constructor() {
@@ -26,8 +26,19 @@ class App extends React.Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    this.unsubscribeFromAuth = auth.onAuthStateChanged((user) => {
-      this.setState({ currentUser: user });
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+      if (userAuth) {
+        const userRef = await createUserProfileDocument(userAuth);
+
+        userRef.onSnapshot((snapshot) => {
+          this.setState({
+            currentUser: snapshot.id,
+            ...snapshot.data(),
+          });
+        });
+      }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
